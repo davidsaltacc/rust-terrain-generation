d5b051d250b11295b6573f75e3160e380983fd88
Coti <108825505+Coti7@users.noreply.github.com>
2024-08-28 01:08:15 +0200

    pls fix while im enjoying vacations

--- terrainGenerator/src/main.rs
+++ terrainGenerator/src/main.rs
@@ -1,33 +1,122 @@
+use wgpu::{RequestAdapterOptions, SurfaceConfiguration};
 use winit::{
     event::*,
-    event_loop::{EventLoop, ControlFlow},
-    window::WindowBuilder,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
 };
 
 fn main() {
-    env_logger::init(); // logs errors when shit goes wrong
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    pollster::block_on(run());
+}
+
+async fn run() {
+    struct State<'a> {
+        surface: wgpu::Surface<'a>,
+        device: wgpu::Device,
+        queue: wgpu::Queue,
+        config: wgpu::SurfaceConfiguration,
+        size: winit::dpi::PhysicalSize<u32>,
+        window: &'a Window,
+    }
+
+    impl<'a> State<'a> {
+        async fn new(window: &'a Window) -> State<'a> {
+            let size = window.inner_size();
+    
+            //some shit for wasm32, made it universal
+            let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+                #[cfg(not(target_arch="wasm32"))]
+                backends: wgpu::Backends::PRIMARY,
+                #[cfg(target_arch="wasm32")]
+                backends: wgpu::Backends::GL,
+                ..Default::default()
+            });
+            
+            let surface = instance.create_surface(window).unwrap();
+    
+            let adapter = instance.request_adapter(
+                &wgpu::RequestAdapterOptions {
+                    power_preference: wgpu::PowerPreference::default(),
+                    compatible_surface: Some(&surface),
+                    force_fallback_adapter: false,
+                },
+            ).await.unwrap();
+       
+            let (device, queue) = adapter.request_device(
+                &wgpu::DeviceDescriptor{
+                    required_features: wgpu::Features::empty(),
+                    required_limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    memory_hints: wgpu::MemoryHints::Performance,
+                    label: None,
+                },
+                None,
+            ).await.unwrap();
+
+            let surface_caps = surface.get_capabilities(&adapter);
+
+            let surface_format = surface_caps.formats.iter()
+                .find(|f| f.is_srgb())
+                .copied()
+                .unwrap_or(surface_caps.formats[0]);
+
+            let config = wgpu::SurfaceConfiguration {
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                format: surface_format,
+                width: size.width,
+                height: size.height,
+                present_mode: wgpu::PresentMode::Fifo,
+                alpha_mode: surface_caps.alpha_modes[0],
+                view_formats: vec![],
+                desired_maximum_frame_latency: 2,
+            };
+
+            return Self {
+                window,
+                surface,
+                device,
+                queue,
+                config,
+                size,
+            };
+        }
+        
+        pub fn window(&self) -> &Window {
+            &self.window
+        }
+
+        fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+            todo!()
+        }
+
+        fn input(&mut self, event: &WindowEvent ) -> bool {
+            todo!()
+        }
+
+        fn update(&mut self) {
+            todo!()
+        }
+
+        fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+            todo!()
+        }
+    }
+
+    let mut state = State::new(&window).await;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
-    
+
         match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            Event::WindowEvent {
-                 event: WindowEvent::KeyboardInput { input, .. },
-                 window_id,
-             } if window_id == window.id() => {
-                 if input.virtual_keycode == Some(VirtualKeyCode::Escape) { 
-                     *control_flow = ControlFlow::Exit
-                 } else if input.virtual_keycode == Some(VirtualKeyCode::W) {
-                    *control_flow = ControlFlow::Exit
-                 }
-             }
-            _ => (),
+            } if window_id == state.window().id() => *control_flow = ControlFlow::Exit,
+             _ => (),
+            }
         }
-    });
+    )
 }
