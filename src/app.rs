8f6691abdca55ed0d708023879885615414e2551
davidsaltacc <d.lumpi@outlook.de>
2024-08-28 21:33:33 +0200

    merged control system and rendering

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,21 @@
 use std::sync::Arc;
+use std::time::{Duration, Instant};
+use std::collections::HashMap;
 
 use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
+use winit::event::*;
 use winit::event_loop::ActiveEventLoop;
 use winit::window::{Window, WindowId};
 
 use crate::wgpu_ctx::WgpuCtx;
+use crate::player;
 
 #[derive(Default)]
 pub struct App<'window> {
+    dt_start: Option<Instant>,
+    dt: Duration,
+    player: Option<player::Player>,
+    keys: HashMap<u8, bool>,
     window: Option<Arc<Window>>,
     wgpu_ctx: Option<WgpuCtx<'window>>,
 }
@@ -17,7 +24,6 @@ impl<'window> ApplicationHandler for App<'window> {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         if self.window.is_none() {
             let win_attr = Window::default_attributes().with_title("wgpu winit example");
-            // use Arc.
             let window = Arc::new(
                 event_loop
                     .create_window(win_attr)
@@ -26,6 +32,7 @@ impl<'window> ApplicationHandler for App<'window> {
             self.window = Some(window.clone());
             let wgpu_ctx = WgpuCtx::new(window.clone());
             self.wgpu_ctx = Some(wgpu_ctx);
+            self.player = Some(player::Player::new());
         }
     }
 
@@ -51,10 +58,32 @@ impl<'window> ApplicationHandler for App<'window> {
             }
             WindowEvent::RedrawRequested => {
                 if let Some(wgpu_ctx) = self.wgpu_ctx.as_mut() {
+                    if self.dt_start.is_some() {
+                        self.dt = self.dt_start.unwrap().elapsed();
+                    }
+                    else {
+                        self.dt = Duration::new(0, 0);
+                    }
+                    
+                    wgpu_ctx.update(self.dt, self.player.as_mut().unwrap().player_position, self.player.as_mut().unwrap().camera_rotation);
+                    
                     wgpu_ctx.draw();
+                    self.player.as_mut().unwrap().update(&self.keys, self.dt.as_secs_f32());
+                    self.dt_start = Some(Instant::now());
+                }
+            }
+            WindowEvent::KeyboardInput {device_id: _, event, is_synthetic} => {
+                if !is_synthetic {
+                    if event.logical_key.to_text().is_some() {
+                        self.keys.insert((event.logical_key.to_text().unwrap().as_bytes()[0]) as u8, event.state == ElementState::Pressed);
+                    }
                 }
             }
             _ => (),
         }
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        self.window.as_mut().unwrap().request_redraw();
     }
 }
