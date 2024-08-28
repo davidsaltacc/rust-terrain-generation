d90f8643cf6622bd9d1a6c905aef3debbfc1c5ec
davidsaltacc <d.lumpi@outlook.de>
2024-08-28 12:23:05 +0200

    i stole this code

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,13 @@
+use crate::app::App;
+use winit::error::EventLoopError;
+use winit::event_loop::{ControlFlow, EventLoop};
 
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+mod app;
+mod wgpu_ctx;
 
-fn main() {
-
-    env_logger::init(); 
-    let event_loop = EventLoop::new(); 
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    
-    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor { backends: wgpu::Backends::all(), ..Default::default() });
-    let surface = unsafe { instance.create_surface(&window) };
-    let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
-        power_preference: wgpu::PowerPreference::default(),
-        compatible_surface: Some(&surface),
-        force_fallback_adapter: false,
-    })).unwrap();
-
-    let (device, queue) = pollster::block_on(adapter.request_device(&wgpu::DeviceDescriptor {
-        label: None,
-        required_features: wgpu::Features::empty(),
-        required_limits: wgpu::Limits::default(),
-        memory_hints: wgpu::MemoryHints::default()
-    }, None,)).unwrap();
-
-    let size = window.inner_size();
-    surface.configure(&device, &wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: surface.get_preferred_format(&adapter).unwrap(),
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Fifo,
-    });
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-    
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            Event::WindowEvent {
-                 event: WindowEvent::KeyboardInput { input, .. },
-                 window_id,
-             } if window_id == window.id() => {
-                 if input.virtual_keycode == Some(VirtualKeyCode::Escape) {
-                     *control_flow = ControlFlow::Exit
-                 }
-             }
-            _ => (),
-        }
-    });
-
+fn main() -> Result<(), EventLoopError> {
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut app = App::default();
+    event_loop.run_app(&mut app)
 }
