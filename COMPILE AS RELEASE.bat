1e241a9277f7de6526d3c159a44aff8f9fcd7d15
davidsaltacc <d.lumpi@outlook.de>
2024-08-28 13:00:46 +0200

    ye

---
+++ COMPILE AS RELEASE.bat
@@ -0,0 +1,1 @@
+cargo rustc --release -- -C link-args=-Wl,--subsystem,windows
--- src/wgpu_ctx.rs
+++ src/wgpu_ctx.rs
@@ -93,7 +93,7 @@ impl<'window> WgpuCtx<'window> {
                     view: &texture_view,
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
                         store: wgpu::StoreOp::Store,
                     },
                 })],
