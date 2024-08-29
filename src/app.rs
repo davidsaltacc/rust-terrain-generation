de64a7a1fca6ef987b3c8ac6b5bdef4fde00ed7b
davidsaltacc <d.lumpi@outlook.de>
2024-08-29 12:55:29 +0200

    mouse locking + movement

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 use std::collections::HashMap;
@@ -9,13 +10,45 @@ use crate::wgpu_ctx::WgpuCtx;
 use crate::player;
 
 #[derive(Default)]
+struct MouseGrabber {
+	last_pos: winit::dpi::PhysicalPosition<f64>,
+	manual_lock: bool,
+}
+
+// winit does not support cursor locking on win10, so we need to make it ourselves
+
+impl MouseGrabber {
+	fn cursor_moved(&mut self, window: &Window, pos: winit::dpi::PhysicalPosition<f64>) {
+		if self.manual_lock {
+			window.set_cursor_position(self.last_pos).unwrap();
+		} else {
+			self.last_pos = pos;
+		}
+	}
+    
+	fn grab(&mut self, window: &Window, grab: bool) {
+		if grab {
+			if window.set_cursor_grab(winit::window::CursorGrabMode::Locked).is_err() {
+				window.set_cursor_grab(winit::window::CursorGrabMode::Confined).unwrap();
+				self.manual_lock = true;
+			}
+		} else {
+			self.manual_lock = false;
+			window.set_cursor_grab(winit::window::CursorGrabMode::None).unwrap();
+		}
+		window.set_cursor_visible(!grab);
+	}
+}
+
+#[derive(Default)]
 pub struct App<'window> {
     dt_start: Option<Instant>,
     dt: Duration,
     player: player::Player,
     keys: HashMap<u8, bool>,
     window: Option<Arc<Window>>,
-    wgpu_ctx: Option<WgpuCtx<'window>>
+    wgpu_ctx: Option<WgpuCtx<'window>>,
+    mouse_grabber: MouseGrabber
 }
 
 impl App<'_> {
@@ -33,6 +66,7 @@ impl<'window> ApplicationHandler for App<'window> {
             let window = Arc::new(event_loop.create_window(win_attr).expect("Error creating window."));
             self.window = Some(window.clone());
             self.wgpu_ctx = Some(WgpuCtx::new(window.clone()));
+            self.mouse_grabber.grab(&window, true);
         }
     }
 
@@ -67,12 +101,32 @@ impl<'window> ApplicationHandler for App<'window> {
             }
             WindowEvent::KeyboardInput {device_id: _, event, is_synthetic} => {
                 if !is_synthetic {
+                    if event.logical_key.to_text().unwrap_or(&"") == "\x1b" {
+                        self.mouse_grabber.grab(self.window.as_mut().unwrap(), false);
+                    }
                     if event.logical_key.to_text().is_some() {
                         self.keys.insert((event.logical_key.to_text().unwrap().as_bytes()[0]) as u8, event.state == ElementState::Pressed);
                     }
                 }
             }
+            WindowEvent::CursorMoved { device_id: _, position } => {
+                self.mouse_grabber.cursor_moved(self.window.as_mut().unwrap(), position);
+            }
+            WindowEvent::MouseInput { device_id: _, state: _, button } => {
+                if button == winit::event::MouseButton::Left {
+                    self.mouse_grabber.grab(self.window.as_mut().unwrap(), true);
+                }
+            }
             _ => (),
+        }
+    }
+
+    fn device_event(&mut self, event_loop: &ActiveEventLoop, device_id: DeviceId, event: DeviceEvent) {
+        match event {
+            DeviceEvent::MouseMotion { delta } => {
+                println!("{}, {}", delta.0, delta.1);
+            }
+            _ => ()
         }
     }
 
