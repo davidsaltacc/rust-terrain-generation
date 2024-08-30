f509f918607eb613c4dafaffb9b6b5edddf6ec02
edy <lazareduard702@gmail.com>
2024-08-30 18:19:24 +0300

    Made a change to compile_as_release.bat

--- compile_as_release.bat
+++ compile_as_release.bat
@@ -1,5 +1,6 @@
 cargo rustc --release -- -Clink-args="/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup"
 :: compile it to an executable that doesn't open a console
 
-move /y "target\release\terrain-generator.exe" "build\"
+if not exist build\ ( mkdir ".\build\" )
+move /y "target\release\terrain-generator.exe" ".\build\"
 explorer.exe .\build\
